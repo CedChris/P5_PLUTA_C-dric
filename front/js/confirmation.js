e4c789cb6545eab508ec85f42c0eea5d7afb575b
CedChris <cedricalanpluta@gmail.com>
2022-03-13 22:11:34 +0100

    ok valide

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,9 +1,4 @@
 // On importe l'ID depuis le localstorage et on le supprime
-function confirmation(){
-
-    const orderNumber = document.getElementById("orderId");
-    orderNumber.innerText = localStorage.getItem("orderId");
-    localStorage.clear();
-}
-
-confirmation();
+const orderNumber = document.getElementById("orderId");
+orderNumber.innerText = localStorage.getItem("orderId");
+localStorage.clear();
